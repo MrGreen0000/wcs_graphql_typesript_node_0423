29bb9de9cf2f4c0b5ee9e74049c5a241c108c0f3
Naji Mouflih <nmouflih@bluesoft-group.com>
2023-04-12 15:17:50 +0200

    update

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,14 @@
+import { buildSchema } from "type-graphql";
 import dataSource from "./dataSource";
-import { ApolloServer, gql } from "apollo-server";
-import Wilder from "./entities/Wilder";
-import Skill from "./entities/Skill";
-
-const typeDefs = gql`
-  type Wilder {
-    id: ID
-    name: String
-    city: String
-    skills: [Skill]
-  }
-  type Skill {
-    id: ID
-    name: String
-    wilders: [Wilder]
-  }
-  type Query {
-    getAllWilders: [Wilder]
-  }
-  type Mutation {
-    createSkill(name: String): Skill
-  }
-`;
-
-const resolvers = {
-  Query: {
-    async getAllWilders(): Promise<Wilder[]> {
-      const wilders = await dataSource.manager.find(Wilder, {
-        relations: { skills: true },
-      });
-      return wilders;
-    },
-  },
-
-  Mutation: {
-    createSkill: async (_: any, args: { name: string }) => {
-      const skillToCreate = new Skill();
-      skillToCreate.name = args.name;
-      return await dataSource.manager.save(Skill, skillToCreate);
-    },
-  },
-};
+import { ApolloServer } from "apollo-server";
+import { WildersResolver } from "./resolvers/WildersResolver";
 
 const start = async (): Promise<void> => {
   await dataSource.initialize();
-  const server = new ApolloServer({ typeDefs, resolvers });
+  const schema = await buildSchema({ resolvers: [WildersResolver] });
+  const server = new ApolloServer({
+    schema,
+  });
   try {
     const { url } = await server.listen({ port: 5000 });
     console.log(`Server ready at ${url}`);
